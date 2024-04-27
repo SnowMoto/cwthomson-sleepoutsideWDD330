b683bf54d7aa0f9351a9a69cf96129ec686aeece
l-gomes-ar <lgaraujo021@gmail.com>
2024-04-27 21:09:03 +0100

    W01 individual activity completed - broken cart

--- src/js/product.js
+++ src/js/product.js
@@ -1,18 +1,19 @@
 import { setLocalStorage, getLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
-const dataSource = new ProductData("ents");
+const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  let cartItems = getLocalStorage("so-cart");
-  //if (cartItems == null || typeof cartItems != Array) {
-  //Check if it exisits then it can be used to pull from storage and if not then it has to be created or Error.
-  if (cartItems == null) {
-    cartItems = [];
-  }
-  cartItems.push(product);
-  setLocalStorage("so-cart", cartItems);
+  let products = [];
+  const soCart = getLocalStorage("so-cart");
+
+  if (soCart !== null)
+    soCart.forEach((item) => products.push(item));
+  
+  products.push(product);
+  setLocalStorage("so-cart", products);
 }
+
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
