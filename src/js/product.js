b3bdc9ea8a200ad8b257e44d0a718a06883a632b
jhtr0226 <johanncraft17@gmail.com>
2024-04-27 21:58:28 -0600

    My cart part

--- src/js/product.js
+++ src/js/product.js
@@ -1,10 +1,16 @@
-import { setLocalStorage } from "./utils.mjs";
+import { setLocalStorage, getLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  let cartItems = [];
+  const cart = getLocalStorage("so-cart");
+  if (cart !== null) {
+    cart.forEach((object) => cartItems.push(object));
+  }
+  cartItems.push(product);
+  setLocalStorage("so-cart", cartItems);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
