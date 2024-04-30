f11ad28bdb13fdc2ff54ec8bffe64d29f2186695
l-gomes-ar <lgaraujo021@gmail.com>
2024-04-30 16:08:59 +0100

    W02: Updated discountDetailPage.js

--- src/js/discountDetailPage.js
+++ src/js/discountDetailPage.js
@@ -2,26 +2,28 @@ import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
-// Get id from attribute data-id in button
-const id = document.querySelector('#addToCart').getAttribute("data-id");
-const product = await dataSource.findProductById(id);
+async function addDiscountDetails(productId) {
+    const product = await dataSource.findProductById(productId);
 
-// Select product card price for adding suggested retail price and discount
-const p = document.querySelector(".product-card__price");
+    // Select product card price for adding suggested retail price and discount
+    const p = document.querySelector(".product-card__price");
+    const span = document.createElement("span");
 
-const span = document.createElement("span");
+    // Get prices
+    const suggestedRetailPrice = product.SuggestedRetailPrice;
+    const finalPrice = product.FinalPrice;
 
-// Calculate discount
-const suggestedRetailPrice = product.SuggestedRetailPrice;
+    // Update .product-card_price with discount details
+    // if RRP bigger than final price
+    if (suggestedRetailPrice > finalPrice) {
+        // Calculate discount and round to hundredths
+        let discountPercent = ((100 * finalPrice) / suggestedRetailPrice) - 100;
+        discountPercent = Math.round(100 * discountPercent) / 100;
 
-const finalPrice = product.FinalPrice;
-
-if (suggestedRetailPrice > finalPrice) {
-    let discountPercent = ((100 * finalPrice) / suggestedRetailPrice) - 100;
-    // Round discountPercent to hundredths
-    discountPercent = Math.round(100 * discountPercent) / 100;
-
-    span.innerHTML = ` <b>${discountPercent}%</b> RRP: <s>$${suggestedRetailPrice}</s>`;
-    p.append(span);
+        span.innerHTML = ` <b>${discountPercent}%</b> RRP: <s>$${suggestedRetailPrice}</s>`;
+        p.append(span);
+    }
 }
 
+const productId = document.querySelector('#addToCart').getAttribute("data-id");
+addDiscountDetails(productId);
