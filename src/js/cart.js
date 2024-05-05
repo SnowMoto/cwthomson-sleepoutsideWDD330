91347ff7b35ab413601dd6636cda6c98274952bf
l-gomes-ar <lgaraujo021@gmail.com>
2024-05-05 17:14:46 +0100

    Remove product from cart feature added

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,4 +1,4 @@
-import { getLocalStorage } from "./utils.mjs";
+import { getLocalStorage, setLocalStorage, updateCartSuperscript } from "./utils.mjs";
 
 function renderCartContents() {
   const cartItems = getLocalStorage("so-cart");
@@ -6,7 +6,14 @@ function renderCartContents() {
   if (cartItems && cartItems.length > 0) {
       const htmlItems = cartItems.map((item) => cartItemTemplate(item));
       document.querySelector(".product-list").innerHTML = htmlItems.join("");
-      
+
+      // Add event listener to all "remove" span elements 
+      document
+        .querySelectorAll(".cart-card__remove")
+        .forEach(span => {
+          span.addEventListener("click", removeEventHandler);
+        });
+
       const total = calculateTotal(cartItems);
       document.querySelector(".cart_total").innerText = `Total: $${total.toFixed(2)}`;
     } else {
@@ -29,12 +36,36 @@ function cartItemTemplate(item) {
   <p class="cart-card__color">${item.Colors[0].ColorName}</p>
   <p class="cart-card__quantity">qty: 1</p>
   <p class="cart-card__price">$${item.FinalPrice}</p>
+  <span data-id="${item.Id}" class="cart-card__remove">🗙</span>
 </li>`;
   return newItem;
 }
 
 function calculateTotal(cartItems) {
   return cartItems.reduce((total, items) => total + items.FinalPrice,0);
+}
+
+function removeFromCart(id) {
+  // Retrieve current cart items from local storage
+  let cartItems = getLocalStorage("so-cart");
+
+  // Find specified product to remove
+  const product = cartItems.filter(item => item.Id === id);
+  // Get only the product object from the array (or the first one, in case there's more than one)
+  const itemToRemove = product[0]; 
+
+  // Delete replaces the property for null, filter the array removing it
+  delete cartItems[cartItems.indexOf(itemToRemove)];
+  cartItems = cartItems.filter(item => item !== null);
+
+  // Set updated cart items list in local storage
+  setLocalStorage("so-cart", cartItems);
+}
+
+function removeEventHandler(e) {
+  removeFromCart(e.target.dataset.id);
+  updateCartSuperscript();
+  renderCartContents();
 }
 
 renderCartContents();
