10c6cc39d1e58f9acedbbaccb91364c3e46e5f7e
l-gomes-ar <lgaraujo021@gmail.com>
2024-05-15 23:05:44 +0100

    updated breadcrumbs styles

--- src/js/product-listing.js
+++ src/js/product-listing.js
@@ -16,7 +16,7 @@ listing.init();
 
 async function getbreadcrumbs() {
     let items = await listing.getNumberItems();
-    const breadcrumbTemplate = `<li><a href="/">Home</a></li><i class="fa fa-home"></i> <li><a href="${window.location.href}" class="aElement">${category.charAt(0).toUpperCase() + listing.category.slice(1)}(Items:${items})</a></li><i class="fa fa-home"></i>`;
+    const breadcrumbTemplate = `<li><a href="/">Home</a></li><i class="fa fa-home"></i> <li><a href="${window.location.href}" class="aElement">${category.charAt(0).toUpperCase() + listing.category.slice(1)} <span class="products-number">${items}</span></a></li><i class="fa fa-home"></i>`;
     document.querySelector(".breadcrumb").innerHTML = breadcrumbTemplate;
     setLocalStorage("breadcrumbsPath", breadcrumbTemplate);
 }
