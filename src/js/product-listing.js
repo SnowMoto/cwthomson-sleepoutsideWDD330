c3c273f6b4227971654b235a3eb4b9946ce86f91
l-gomes-ar <lgaraujo021@gmail.com>
2024-05-16 15:30:17 +0100

    Rename ProductData to ExternalServices in all files

--- src/js/product-listing.js
+++ src/js/product-listing.js
@@ -1,4 +1,4 @@
-import ProductData from "./ProductData.mjs";
+import ExternalServices from "./ExternalServices.mjs";
 import ProductListing from "./ProductList.mjs";
 import { loadHeaderFooter, getParams, setLocalStorage } from "./utils.mjs";
 
@@ -6,7 +6,7 @@ loadHeaderFooter();
 
 const category = getParams("category");
 
-const dataSource = new ProductData();
+const dataSource = new ExternalServices();
 const listElement = document.querySelector(".product-list");
 
 const listing = new ProductListing(category, dataSource, listElement);
